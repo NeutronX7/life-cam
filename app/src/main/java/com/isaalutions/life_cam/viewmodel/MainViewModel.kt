0bc9ca501922703272b7a9e6b01c00e143773ed9
NeutronX7 <isaacgriv7@gmail.com>
2026-01-20 21:12:22 -0600

    Feat: Added Webrtc files
    - Prepared environment to use in the UI for implementation and finish the app
    - Added Audio manager, rtc client, observer, peer observer and sdp observer

--- app/src/main/java/com/isaalutions/life_cam/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/isaalutions/life_cam/viewmodel/MainViewModel.kt
@@ -1,9 +1,11 @@
 package com.isaalutions.life_cam.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.isaalutions.life_cam.remote.FirebaseClient
 import com.isaalutions.life_cam.utils.MatchState
+import com.isaalutions.life_cam.webrtc.RTCAudioManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
@@ -11,9 +13,18 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val firebaseClient: FirebaseClient
+    private val firebaseClient: FirebaseClient,
+    private val application: Application
 ) : ViewModel() {
     var matchState: MutableStateFlow<MatchState> = MutableStateFlow(MatchState.NewState)
+    private val rtcAudioManager by lazy {
+        RTCAudioManager.create(application)
+    }
+
+    init {
+        rtcAudioManager.setDefaultAudioDevice(RTCAudioManager.AudioDevice.SPEAKER_PHONE)
+    }
+
     fun permissionsGranted() {
         firebaseClient.observerUserStatus { status ->
             matchState.value = status
